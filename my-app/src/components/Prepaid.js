833146eee3e0981542e798662708b1a2bd927013
CourtneyYon <18955932@rga-hlet325-34.stb.sun.ac.za>
2018-10-25 13:55:30 +0200

    Update my-app/src/components/Prepaid.js

--- my-app/src/components/Prepaid.js
+++ my-app/src/components/Prepaid.js
@@ -1,3 +1,6 @@
+// Description: Prepaid accounts for buying and folding of accounts
+// Date Modified: 24 October 2018
+
 import React from 'react';
 import {Link} from 'react-router-dom';
 import ReactDOM from "react-dom";
@@ -9,24 +12,24 @@ const Prepaid = () => (
         <h1 className="text-center">PREPAID AND UTILTIY OPTIONS</h1>
 
         <h3>Airtime</h3>
-<Container className="text-center">
+        <Container className="text-center">
         <Form>
         <FormGroup>
           <Label for="network">Network Provider</Label>
           <Input type="select" name="select" id="exampleSelect">
-            <option>Cell C</option>
-            <option>MTN</option>
-            <option>Telkom</option>
-            <option>Virgin Mobile</option>
-            <option>Vodacom</option>
+               <option>Cell C</option>
+               <option>MTN</option>
+               <option>Telkom</option>
+               <option>Virgin Mobile</option>
+               <option>Vodacom</option> //different options for better experience
           </Input>
         </FormGroup>
 
         <br  />
         <InputGroup>
-        <InputGroupAddon addonType="prepend">R</InputGroupAddon>
-        <Input placeholder="Amount" type="number" step="1" />
-        <InputGroupAddon addonType="append">.00</InputGroupAddon>
+            <InputGroupAddon addonType="prepend">R</InputGroupAddon>
+            <Input placeholder="Amount" type="number" step="1" />
+            <InputGroupAddon addonType="append">.00</InputGroupAddon>
       </InputGroup>
     </Form><br/>
 
@@ -44,7 +47,8 @@ const Prepaid = () => (
           </Input>
         </FormGroup>
         <br  />
-        <InputGroup>
+
+      <InputGroup>
         <InputGroupAddon addonType="prepend">R</InputGroupAddon>
         <Input placeholder="Amount" type="number" step="1" />
         <InputGroupAddon addonType="append">.00</InputGroupAddon>
@@ -56,25 +60,25 @@ const Prepaid = () => (
       <FormGroup>
         <Label for="network">Electricity Provider</Label>
         <Input type="select" name="select" id="exampleSelect">
-          <option>City of Cape Town Metropolitan Municipality</option>
-          <option>City of Johannesburg Metropolitan Municipality</option>
-          <option>Mangaung Metropolitan Municipality</option>
-          <option>Nelson Mandela Bay Metropolitan Municipality</option>
-          <option>City of Tshwane Metropolitan Municipality</option>
+             <option>City of Cape Town Metropolitan Municipality</option>
+             <option>City of Johannesburg Metropolitan Municipality</option>
+             <option>Mangaung Metropolitan Municipality</option>
+             <option>Nelson Mandela Bay Metropolitan Municipality</option>
+             <option>City of Tshwane Metropolitan Municipality</option>
         </Input>
       </FormGroup>
       <br  />
-      <InputGroup>
-      <InputGroupAddon addonType="prepend">R</InputGroupAddon>
-      <Input placeholder="Amount" type="number" step="1" />
-      <InputGroupAddon addonType="append">.00</InputGroupAddon>
-      </InputGroup>
-      </Form><br/>
+        <InputGroup>
+             <InputGroupAddon addonType="prepend">R</InputGroupAddon>
+             <Input placeholder="Amount" type="number" step="1" />
+             <InputGroupAddon addonType="append">.00</InputGroupAddon>
+        </InputGroup>
+            </Form><br/>
       <Button>Advance & Submit</Button>
-      <br/>
-      <br/>
+            <br/>
+            <br/>
       <Button>Abort Mission..</Button>
-</Container>
+    </Container>
       </div>
     );
 
