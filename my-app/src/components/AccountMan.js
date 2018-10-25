0fe53518b9acf2f88bbf8b59ac503daa89beff4e
CourtneyYon <18955932@rga-hlet325-34.stb.sun.ac.za>
2018-10-25 22:27:16 +0200

    Minor changes to comments

--- my-app/src/components/AccountMan.js
+++ my-app/src/components/AccountMan.js
@@ -7,14 +7,14 @@ import reactCSS from 'reactcss'
 import { Table } from 'reactstrap';
 // import logo from '../logo.jpeg'
 
-const AccountMan = () => ( //for account management functionality
+const AccountMan = () => (
   <div>
           <div className="split-left">
-            <h1> My Personal Account </h1> //personal account linkage
+            <h1> My Personal Account </h1>
             <a href="/PersonalAcc" className="button-personal-acc"><button className="personal-acc" >Enter</button></a>
           </div>
           <div className="split-right">
-            <h1> My Business Account</h1> //business account linkage
+            <h1> My Business Account</h1> 
             <a href="/BusinessAcc" className="button-business-acc"><button className="business-acc">Enter</button></a>
           </div>
   </div>
