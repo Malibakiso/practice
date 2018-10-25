87e6a057ef15edc233995e4391307578c2452e2e
CourtneyYon <18955932@rga-hlet325-34.stb.sun.ac.za>
2018-10-25 14:10:11 +0200

    Changes to Inte Account Structure

--- my-app/src/components/InterAccount.js
+++ my-app/src/components/InterAccount.js
@@ -1,3 +1,6 @@
+// Description: This page was inserted for Inter-Account Section
+// Last Modified: 25 October 2018
+
 import React from 'react';
 import {Link} from 'react-router-dom';
 import ReactDOM from "react-dom";
@@ -6,42 +9,43 @@ import { Button, Container, Form, FormGroup, Label, Input, InputGroup, FormText,
 const InterAccount = () => (
 
   <div>
-  <h1>INTER-ACCOUNT TRANSFERS</h1>
-
+   <h1>INTER-ACCOUNT TRANSFERS</h1>
    <h3>Transfer in between your own Accounts</h3>
-<Container>
-  <FormGroup>
-    <Label for="text">From which Account</Label>
-    <Input type="select" name="accType" id="accType" placeholder="Account type">
-      <option>Cheque Account</option>
-      <option>Savings Account</option>
-      <option>Credit Account</option>
-    </Input>
-  </FormGroup>
+   <Container>
+      <FormGroup>
+               <Label for="text">From which Account</Label>
+               <Input type="select" name="accType" id="accType" placeholder="Account type">
+                    <option>Cheque Account</option>
+                    <option>Savings Account</option>
+                    <option>Credit Account</option>
+               </Input>
+      </FormGroup>
+
   <FormGroup>
     <Label for="text">To which Account</Label>
-    <Input type="select" name="accType" id="accType" placeholder="Account type">
-      <option>Cheque Account</option>
-      <option>Savings Account</option>
-      <option>Credit Account</option>
+        <Input type="select" name="accType" id="accType" placeholder="Account type">
+           <option>Cheque Account</option>
+            <option>Savings Account</option>
+            <option>Credit Account</option>
     </Input>
-  </FormGroup>
-  <InputGroup>
-  <InputGroupAddon addonType="prepend">R</InputGroupAddon>
-  <Input placeholder="Amount" type="number" step="1" />
-  <InputGroupAddon addonType="append">.00</InputGroupAddon>
-</InputGroup>
-<br/>
-<FormGroup>
-<Label for="text">Reference</Label>
-<Input type="textarea" name="text" id="text" placeholder="Your personal reference?" />
-</FormGroup>
-<br/>
-<Button>Submit & Transfer</Button>
-<br/>
-<br/>
-<Button>Abort Mission..</Button>
-</Container>
+    </FormGroup>
+               <InputGroup>
+                   <InputGroupAddon addonType="prepend">R</InputGroupAddon>
+                   <Input placeholder="Amount" type="number" step="1" />
+                   <InputGroupAddon addonType="append">.00</InputGroupAddon>
+               </InputGroup>
+         <br/>
+
+     <FormGroup>
+              <Label for="text">Reference</Label>
+              <Input type="textarea" name="text" id="text" placeholder="Your personal reference?" />
+     </FormGroup>
+        <br/>
+           <Button>Submit & Transfer</Button>
+        <br/>
+        <br/>
+           <Button>Abort Mission..</Button>
+   </Container>
 </div>
     );
 
